43c39ac9d9cd88f764f856a5804394f2b49158d5
Byron-Poveda <119381029+Byron-Poveda@users.noreply.github.com>
2023-03-22 16:46:47 +0000

    Deploying to gh-pages from @ Byron-Poveda/Proyecto_html_y_css_goit@76995a8732a7be029430895143637023db9fae6f 🚀

---
+++ index.9a3a1ba1.js
@@ -0,0 +1,2 @@
+!function(){var o={openModal:document.querySelector("[data-modal-location-open]"),closeModal:document.querySelector("[data-modal-location-close]"),modallocation:document.querySelector("[data-modal-location]")};function e(){o.modallocation.classList.toggle("is-hidden")}o.openModal.addEventListener("click",e),o.closeModal.addEventListener("click",e)}();
+//# sourceMappingURL=index.9a3a1ba1.js.map
---
+++ index.dac0fed8.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModal:document.querySelector("[data-modal-location-open]"),closeModal:document.querySelector("[data-modal-location-close]"),modallocation:document.querySelector("[data-modal-location]")};function e(){o.modallocation.classList.toggle("is-hidden")}o.openModal.addEventListener("click",e),o.closeModal.addEventListener("click",e)})();
+//# sourceMappingURL=index.dac0fed8.js.map
